13a55e8aadfbf4783d1b52a4f76cb1cbbf63c1f3
Shekhar <sm.contactmeon@gmail.com>
2022-04-04 23:46:22 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Navbar from "./components/navbar";
 import MainRoutes from "./Router/Routes";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <MainRoutes/>
+      <Footer/>
       </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="container-fluid">
+      <div className="footer">
+        <div className="footer-items ">
+          <img className='footer-title' width="120px" src="\assets\footer-logo.svg" alt="logo" />
+          <p className='footer-btn'>join the community</p>
+        </div>
+        <div className="footer-items">
+          <h3>GET INVOLVED</h3>
+          <p> Contribution BibleB </p>
+          <p> Add New Movie</p>
+          <p> Add New TV Show </p>
+        </div>
+        <div className="footer-items">
+          <h3>COMMUNITY</h3>
+          <p> Guidelines</p>
+          <p> Discussions</p>
+          <p> Leaderboard </p>
+          <p> Twitter </p>
+        </div>
+        <div className="footer-items">
+          <h3>LEGAL</h3>
+          <p>Terms of Use</p>
+          <p>API Terms of Use</p>
+          <p> Support Forums </p>
+        </div>
+        <div className="footer-items">
+          <h3>THE BASICS</h3>
+          <p> About TMDB </p>
+          <p> Contact Us </p>
+          <p> Support Forums </p>
+          <p> API </p>
+          <p> System Status </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
